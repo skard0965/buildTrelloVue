a688d46323e0fccaa5821f058a6cf77d325809c1
Shadi Kardan <skard0965@gmail.com>
2020-05-21 14:45:12 -0400

    Extracting components

---
+++ src/components/ColumnTask.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="task"
+    draggable @dragover.prevent @dragenter.prevent @drop.stop="moveTaskOrColumn($event, column.tasks,columnIndex, taskIndex)"
+    @dragstart="pickupTask($event,taskIndex,columnIndex)" @click="goToTask(task)">
+
+        <span class="w-full flex-no-shrink font-bold">
+        {{task.name}}
+        </span>
+        <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
+        {{task.description}}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    task: {
+      type: Object,
+      required: true
+    },
+    taskIndex: {
+      type: Number,
+      required: true
+    },
+    columnIndex: {
+      type: Number,
+      required: true
+    },
+    column: {
+      type: Object,
+      required: true
+    },
+    board: {
+      type: Object,
+      required: true      
+    }
+  },
+  methods:{
+    pickupTask (e, taskIndex, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dataTransfer = 'move'
+
+      e.dataTransfer.setData('from-task-index', taskIndex)
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+      e.dataTransfer.setData('type', 'task')
+    },
+    goToTask (task) {
+      this.$router.push({ name: 'task', params: { id: task.id } })
+    },
+    moveTaskOrColumn (e, toTasks, toColumnIndex, toTaskIndex) {
+      const type = e.dataTransfer.getData('type')
+      if (type === 'task') {
+        this.moveTask(e, toTasks, toTaskIndex !== undefined ? toTaskIndex : toTasks.length)
+      } else {
+        this.moveColumn(e, toColumnIndex)
+      }
+    },
+  }
+
+}
+</script>
+
+<style>
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+
+</style>
